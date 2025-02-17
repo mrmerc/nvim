15314fe5b037bf767150a1741d639556f1cf755f
Vasiliy Andreev <vasiliy.andreev@1win.pro>
2025-02-17 17:40:10 +0100

    norm

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,5 +1,5 @@
 -- Highlight yandked text
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 	callback = function()
 		vim.highlight.on_yank({ priority = 250 })
