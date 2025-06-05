20f9b2b14eceeedc073cb9d2dacdc1a714466541
Vasiliy Andreev <merc.l33t@gmail.com>
2025-06-05 14:29:45 +0200

    diffview keybind

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -5,49 +5,51 @@ return {
 	},
 	keys = {
 		{ "<leader>gd", "<cmd>DiffviewOpen<CR>", mode = "n", desc = "Open Diffview" },
-		{ "<leader>gf", "<cmd>DiffviewFileHistory %<CR>", mode = "n", desc = "Open Diffview History" },
+		{ "<leader>gF", "<cmd>DiffviewFileHistory<CR>", mode = "n", desc = "Open Diffview History" },
+		{ "<leader>gf", "<cmd>DiffviewFileHistory %<CR>", mode = "n", desc = "Open Diffview History (file)" },
 	},
-	opts = {
-		enhanced_diff_hl = true,
-		view = {
-			merge_tool = {
-				layout = "diff1_plain",
-			},
-		},
-		file_panel = {
-			win_config = {
-				position = "bottom",
-				heigth = 20,
-			},
-		},
-		hooks = {
-			view_opened = function()
-				vim.api.nvim_tabpage_set_var(0, "name", "Diffview")
-			end,
-			diff_buf_read = function()
-				vim.opt_local.wrap = false
-			end,
-		},
-		keymaps = {
-			-- disable_defaults = false,
+	config = function()
+		require("diffview").setup({
+			enhanced_diff_hl = true,
 			view = {
-				-- {
-				-- 	"n",
-				-- 	"<leader>b",
-				-- 	require("diffview.actions").focus_files,
-				-- 	{ desc = "Bring focus to the file panel" },
-				-- },
-				-- {
-				-- 	"n",
-				-- 	"<leader>e",
-				-- 	require("diffview.actions").toggle_files,
-				-- 	{ desc = "Toggle the file panel." },
-				-- },
-				{ "n", "q", "<CMD>DiffviewClose<CR>", { desc = "Close Diffview" } },
+				merge_tool = {
+					layout = "diff1_plain",
+				},
 			},
 			file_panel = {
-				{ "n", "q", "<CMD>DiffviewClose<CR>", { desc = "Close Diffview" } },
+				win_config = {
+					position = "bottom",
+					heigth = 20,
+				},
 			},
-		},
-	},
+			hooks = {
+				view_opened = function()
+					vim.api.nvim_tabpage_set_var(0, "name", "Diffview")
+				end,
+				diff_buf_read = function()
+					vim.opt_local.wrap = false
+				end,
+			},
+			keymaps = {
+				view = {
+					{
+						"n",
+						"<leader>cc",
+						require("diffview.actions").conflict_choose("ours"),
+						{ desc = "Choose the Current change" },
+					},
+					{
+						"n",
+						"<leader>ci",
+						require("diffview.actions").conflict_choose("theirs"),
+						{ desc = "Choose the Incoming change" },
+					},
+					{ "n", "q", "<CMD>DiffviewClose<CR>", { desc = "Close Diffview" } },
+				},
+				file_panel = {
+					{ "n", "q", "<CMD>DiffviewClose<CR>", { desc = "Close Diffview" } },
+				},
+			},
+		})
+	end,
 }
